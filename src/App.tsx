5416a09835eb1bae30819fb2738c751424db8bcb
biruk habte <biruk.habte-ug@aau.edu.et>
2026-03-12 13:59:15 +0300

    hero sec added

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900">
+    <div className="min-h-screen custom-bg">
       <AnimatePresence mode="wait">
         <Navbar />
         <main>
