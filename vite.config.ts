c90ded5eb172a49bb02d94d06b4c990ac0b16ae7
biruk habte <biruk.habte-ug@aau.edu.et>
2026-03-18 12:36:49 +0300

    build configuration - remove Terser dependency"

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: 'terser'
-  },
-  base: './'
+    outDir: 'dist'
+  }
 })
 
 
